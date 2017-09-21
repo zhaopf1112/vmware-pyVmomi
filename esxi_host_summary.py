5016a546e344ad6e95e23e779450e1f2afc627a9
zhaopf1112 <zhaopengfei1112@163.com>
2017-09-21 18:23:50 +0800

    first push esxi scripts

---
+++ esxi_host_summary.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import os
+import sys
+import yaml
+import commands
+import logging
+import logging.handlers
+import atexit
+import datetime
+import time
+
+from pyVim import connect
+from pyVmomi import vmodl
+from pyVmomi import vim
+
+try:
+    esxi_ci = sys.argv[1]
+    esxi_ip = sys.argv[2]
+except Exception, e:
+    print "False, Usage: ", sys.argv[0], "esxi_ci esxi_ip"
+    sys.exit(99)
+
+LOG_FILE = "/var/log/esxi.log"
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=20 * 1024 * 1024, backupCount=5)
+fmt = "%(asctime)s - %(name)s - [%(filename)s:%(lineno)s] - %(levelname)s - %(message)s "
+formatter = logging.Formatter(fmt)
+handler.setFormatter(formatter)
+logger = logging.getLogger(esxi_ci)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+param_yaml = os.path.join(os.path.dirname(__file__), "esxi.yaml").replace('\\', '/')
+p_esxi = yaml.load(file(param_yaml, "r"))
+esxi_user = p_esxi.get('esxi_user')
+esxi_pass = p_esxi.get('esxi_pass')
+esxi_port = p_esxi.get('esxi_port')
+
+
+def esxi_shutdown(host, ip):
+    try:
+        logger.debug('Host: %s, IP: %s, Port: %s' % (host, ip, esxi_port))
+        esxi_instance = connect.SmartConnect(host=ip, user=esxi_user, pwd=esxi_pass, port=int(esxi_port))
+        if not esxi_instance:
+            msg = 'Could not connect to the host: %s, %s' % (host, ip)
+            logger.error(msg)
+            return False, msg
+        atexit.register(connect.Disconnect, esxi_instance)
+        
+        content = esxi_instance.RetrieveContent()
+        object_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.HostSystem], True)
+        l_hosts = object_view.view
+        object_view.Destroy()
+        print l_hosts[0].summary
+        
+        
+                
+##       Enter MaintenanceMode
+#        try:
+#            main_mode = l_hosts[0].EnterMaintenanceMode_Task(5)
+#            logger.debug('EnterMaintenanceMode_Task excuted'+str(main_mode))
+#        except vim.fault.Timedout as err:
+#            msg = "Caught vim fault: %s" % err.msg
+#            logger.exception(msg)
+#            return False, msg 
+#        time.sleep(30)
+##       Shutdown esxi host
+#        try:
+#            shutdown_ = l_hosts[0].ShutdownHost_Task(True)
+#            logger.debug('shutdown esxi host'+str(shutdown_))
+#        except vmodl.fault.NotSupported as err:
+#            msg = "Caught vmodl fault: %s" % err.msg
+#            logger.exception(msg)
+#            return False, msg
+#
+    except vmodl.MethodFault as err:
+        msg = "Caught vmodl fault: %s" % err.msg
+        logger.exception(msg)
+        return False, msg
+    except Exception as err:
+        msg = "Caught exception: %s" % str(err)
+        logger.exception(msg)
+        return False, msg
+
+
+# Start program
+if __name__ == "__main__":
+    esxi_shutdown(esxi_ci, esxi_ip)
+
